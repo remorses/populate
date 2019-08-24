bdde5e416f7a64d3a225441d82111677f3eea3bb
bump-version <bump@version.com>
2019-08-24 13:32:39 +0000

    0.0.4
    [skip ci]

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ NAME = 'populate'
 setup(
     name=NAME,
     # [bump]
-    version='0.0.3',
+    version='0.0.4',
     description=NAME,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
