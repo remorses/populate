18a12814aad645b37443e173dab3119b91c91d4c
remorses <beats.by.morse@gmail.com>
2019-08-29 11:40:33 +0200

    added do_eval and rm strip

--- populate/__init__.py
+++ populate/__init__.py
@@ -14,35 +14,17 @@ import datetime
 INDICATOR_START = '${{'
 INDICATOR_END = '}}'
 
-def populate_string(yaml_string, data={}, do_repr=False):
+def populate_string(yaml_string, data={}, do_repr=False, do_eval=True):
     """
     max one {{  }} per line!
     """
-
-    def xeval(expr, data):
-        try:
-            return eval(expr, {
-                **{name: getattr(funcy, name) for name in funcy.__all__},
-                'random': random,
-                'env': dotdict(**os.environ),
-                'operator': operator,
-                **data,
-                'data':data,
-                'datetime':datetime.datetime,
-                'timedelta':datetime.timedelta,
-                'json':json
-            })
-        except Exception as e:
-            print(f'error `{e}` in xeval for expression "{expr}"')
-            raise e from None
-            
     def replace_in_line(line):
         while INDICATOR_START in line and INDICATOR_END in line and not ('#' in line and line.index('#') < line.index(INDICATOR_START)): 
             begin = line.index(INDICATOR_START)
             end = line.index(INDICATOR_END, begin)
-            variable_name = line[begin:end].strip().replace(INDICATOR_START,'').replace(INDICATOR_END,'').strip()
+            variable_name = line[begin:end].replace(INDICATOR_START,'').replace(INDICATOR_END,'')
             try:
-                value = xeval(variable_name, data)
+                value = xeval(variable_name, data) if do_eval else variable_name
                 line = (
                     line[:begin] +
                     # value if isinstance(value, (int, float)) else repr(value) +
@@ -62,7 +44,7 @@ def populate_string(yaml_string, data={}, do_repr=False):
                 else:
                     index = part.index(INDICATOR_END)
                     expr = part[0:index]
-                    expr = '\n'.join([line.strip() for line in expr.splitlines()])
+                    expr = '\n'.join([line for line in expr.splitlines()])
                     # print(repr(expr))
                     # print(expr)
                     value = xeval(expr, data)
@@ -78,3 +60,19 @@ def populate_string(yaml_string, data={}, do_repr=False):
 
 
 
+
+def xeval(expr, data):
+    try:
+        return eval(expr, {
+            **{name: getattr(funcy, name) for name in funcy.__all__},
+            'random': random,
+            'env': dotdict(**os.environ),
+            'operator': operator,
+            **data,
+            'data':data,
+            'datetime':datetime.datetime,
+            'timedelta':datetime.timedelta,
+            'json':json
+        })
+    except Exception as e:
+        raise Exception(f'error `{e}` in xeval for expression "{expr}"') from None
